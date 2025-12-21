96497d50cfe21d7a016a2f09cef5b2f1b0eb4fb7
baseng1337 <topupgameku.id@gmail.com>
2025-12-21 11:36:45 +0700

    Create receiver.php

---
+++ receiver.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$secret = 'wtf';
+$file   = 'result.json';
+
+if (isset($_POST['action']) && $_POST['action'] === 'register_site') {
+    if ($_POST['secret'] !== $secret) exit;
+
+    $domain = filter_var($_POST['domain'], FILTER_SANITIZE_URL);
+    $key    = parse_url($domain, PHP_URL_HOST) ?: $domain;
+    
+    $data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+    
+    // Jika domain belum ada, tambahkan. Jika sudah ada, jangan timpa password yang sudah disimpan manual
+    if (!isset($data[$key])) {
+        $data[$key] = [
+            'domain'   => $domain,
+            'api_user' => '',
+            'api_pass' => ''
+        ];
+        file_put_contents($file, json_encode($data));
+    }
+}
