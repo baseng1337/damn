e736b912b1d68d78e1557fa1f2989eb7238797fa
baseng1337 <topupgameku.id@gmail.com>
2025-12-21 18:57:09 +0700

    Update receiver.php

--- receiver.php
+++ receiver.php
@@ -11,20 +11,22 @@ if (isset($_POST['action']) && $_POST['action'] === 'register_site') {
     $domain = filter_var($_POST['domain'], FILTER_SANITIZE_URL);
     $key    = parse_url($domain, PHP_URL_HOST) ?: $domain;
     
+    // Tangkap data tambahan
+    $api_user = isset($_POST['api_user']) ? $_POST['api_user'] : '';
+    $api_pass = isset($_POST['api_pass']) ? $_POST['api_pass'] : '';
+    
     $data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
     
     if (!is_array($data)) $data = [];
 
-    // Jika domain belum ada, tambahkan.
-    if (!isset($data[$key])) {
-        $data[$key] = [
-            'domain'   => $domain,
-            'api_user' => '',
-            'api_pass' => '',
-            'date_added' => date('Y-m-d H:i:s')
-        ];
-        // Tambahkan JSON_PRETTY_PRINT agar rapi
-        file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
-    }
+    // Update atau tambah data baru
+    $data[$key] = [
+        'domain'   => $domain,
+        'api_user' => $api_user,
+        'api_pass' => $api_pass,
+        'date_added' => date('Y-m-d H:i:s')
+    ];
+    
+    file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
 }
 ?>
